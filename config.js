269a620c44a28fd2a1591c6bc1701133ba41804a
lyaaa.cyber <juliavps1244@gmail.com>
2026-09-16 14:00:27 -0300

    Adiciona configuração pública do Supabase

---
+++ config.js
@@ -0,0 +1,6 @@
+// CONFIGURAÇÃO PÚBLICA DO SUPABASE
+// A Publishable Key (ou anon key) pode ficar no front-end. NUNCA coloque a service_role key aqui.
+window.SOULY_CONFIG = {
+  SUPABASE_URL: "COLE_AQUI_A_PROJECT_URL",
+  SUPABASE_PUBLISHABLE_KEY: "COLE_AQUI_A_PUBLISHABLE_KEY"
+};
